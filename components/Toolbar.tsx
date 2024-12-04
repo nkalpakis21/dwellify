2a4884d6cdb64e1e9ef7f30f0c91598452de913e
Nicholas Kalpakis <nkalpakis21@gmail.com>
2024-12-04 10:53:23 -0500

    Add contact in navbar.

--- components/Toolbar.tsx
+++ components/Toolbar.tsx
@@ -96,7 +96,7 @@ export function ToolbarCombined() {
           <ul className="flex space-x-4">
             <li><Link href="/photos" className="text-blue-600 hover:text-blue-800">Photos</Link></li>
             {/* <li><Link href="/about" className="text-blue-600 hover:text-blue-800">About</Link></li> */}
-            {/* <li><Link href="/contact" className="text-blue-600 hover:text-blue-800">Contact</Link></li> */}
+            <li><Link href="/contact" className="text-blue-600 hover:text-blue-800">Contact</Link></li>
           </ul>
         </nav>
       </div>
