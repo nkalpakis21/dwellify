188c85364cd7b8cd6127801c4ff5060fa6ce908c
Nicholas Kalpakis <nkalpakis21@gmail.com>
2024-12-09 20:39:32 -0500

    Add image domain in next config.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['dwellify.xyz'],
+  },
+}
 
-export default nextConfig;
+module.exports = nextConfig
+
